a0057e6ecbe53962db37478bc6b4129a6c132082
EdsonArchela <edi.eeq@gmail.com>
2021-02-14 10:51:24 -0300

    first commit

---
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -0,0 +1,35 @@
+import { Router } from 'express';
+import { celebrate, Segments, Joi } from 'celebrate';
+import UsersController from '../controllers/UsersController';
+
+// import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+
+const usersRouter = Router();
+const usersController = new UsersController();
+
+usersRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+      password: Joi.string().required(),
+      comission: Joi.number(),
+    },
+  }),
+  usersController.create,
+);
+
+// usersRouter.use(ensureAuthenticated);
+
+// usersRouter.patch(
+//   '/cpf',
+//   celebrate({
+//     [Segments.BODY]: {
+//       cpf: Joi.string().required(),
+//     },
+//   }),
+//   usersController.update,
+// );
+
+export default usersRouter;
